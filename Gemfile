f40f9b668ab2bd0ec0bfa9df28bf28ced6601d4a
doitumura <1993nakayama@gmail.com>
2022-06-19 17:35:39 +0900

    rubocopの指摘を修正

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 source 'https://rubygems.org'
+gem 'pg'
 gem 'sinatra'
 gem 'sinatra-contrib'
-gem 'pg'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ def read_memos(connection)
   @memos = []
   connection.exec('select * from memos') do |result|
     result.each do |row|
-      memo = {id: row["id"], title: row["title"], content: row["content"]}
+      memo = { id: row['id'], title: row['title'], content: row['content'] }
       @memos.push memo
     end
   end
@@ -26,38 +26,38 @@ def read_memo(memo_id, connection)
   connection.prepare('read_memo', 'select * from memos where id = $1')
   connection.exec_prepared('read_memo', [memo_id]) do |result|
     result.each do |row|
-      memo = {id: row["id"], title: row["title"], content: row["content"]}
+      memo = { id: row['id'], title: row['title'], content: row['content'] }
     end
   end
-  connection.exec("DEALLOCATE read_memo")
+  connection.exec('DEALLOCATE read_memo')
   memo
 end
 
 def write_memo(title, content, connection)
   connection.prepare('write_memo', "insert into memos(id, title, content) values(nextval('memos_sequence'), $1, $2);")
   connection.exec_prepared('write_memo', [title, content])
-  connection.exec("DEALLOCATE write_memo")
+  connection.exec('DEALLOCATE write_memo')
 end
 
 def edit_memo(memo_id, title, content, connection)
-  connection.prepare('edit_memo', "update memos set(title, content) = ($1, $2) where id = $3;")
+  connection.prepare('edit_memo', 'update memos set(title, content) = ($1, $2) where id = $3;')
   connection.exec_prepared('edit_memo', [title, content, memo_id])
-  connection.exec("DEALLOCATE edit_memo")
+  connection.exec('DEALLOCATE edit_memo')
 end
 
 def delete_memo(memo_id, connection)
-  connection.prepare('delete_memo', "delete from memos where id = $1;")
+  connection.prepare('delete_memo', 'delete from memos where id = $1;')
   connection.exec_prepared('delete_memo', [memo_id])
-  connection.exec("DEALLOCATE delete_memo")
+  connection.exec('DEALLOCATE delete_memo')
 end
 
-if !defined?(connection)
-  connection = PG.connect(dbname:'memo_db')
+unless defined?(connection)
+  connection = PG.connect(dbname: 'memo_db')
 
   connection.exec("select tablename from pg_tables where tablename='memos';") do |result|
-    if result.ntuples == 0
-      connection.exec("create table memos(id serial primary key, title varchar, content varchar);")
-      connection.exec("create sequence memos_sequence start 1 increment 1;")
+    if result.ntuples.zero?
+      connection.exec('create table memos(id serial primary key, title varchar, content varchar);')
+      connection.exec('create sequence memos_sequence start 1 increment 1;')
     end
   end
 end
