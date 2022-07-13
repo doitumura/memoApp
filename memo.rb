6f0540afe492767f222673de4906d465715a2b24
doitumura <1993nakayama@gmail.com>
2022-07-13 13:06:42 +0900

    sql文ではsqlのキーワードを大文字にするように修正

--- memo.rb
+++ memo.rb
@@ -6,12 +6,12 @@ class Memo
   def initialize
     self.connection = PG.connect(dbname: 'memo_db')
 
-    connection.exec("CREATE TABLE IF NOT EXISTS memos(ID SERIAL PRIMARY KEY, TITLE VARCHAR, CONTENT VARCHAR);")
+    connection.exec("CREATE TABLE IF NOT EXISTS memos(id SERIAL PRIMARY KEY, title VARCHAR, content VARCHAR);")
   end
 
   def read_all
     memos = []
-    connection.exec('SELECT * FROM MEMOS ORDER BY ID') do |result|
+    connection.exec('SELECT * FROM memos ORDER BY id') do |result|
       result.each do |row|
         memo = { id: row['id'], title: row['title'], content: row['content'] }
         memos.push memo
@@ -22,31 +22,31 @@ class Memo
 
   def read(memo_id)
     memo = {}
-    connection.prepare('read_memo', 'SELECT * FROM MEMOS WHERE ID = $1')
+    connection.prepare('read_memo', 'SELECT * FROM memos WHERE id = $1')
     connection.exec_prepared('read_memo', [memo_id]) do |result|
       result.each do |row|
         memo = { id: row['id'], title: row['title'], content: row['content'] }
       end
     end
-    connection.exec('DEALLOCATE READ_MEMO')
+    connection.exec('DEALLOCATE read_memo')
     memo
   end
 
   def write(title, content)
-    connection.prepare('write_memo', "INSERT INTO MEMOS(TITLE, CONTENT) VALUES($1, $2);")
+    connection.prepare('write_memo', "INSERT INTO memos(title, content) VALUES($1, $2);")
     connection.exec_prepared('write_memo', [title, content])
-    connection.exec('DEALLOCATE WRITE_MEMO')
+    connection.exec('DEALLOCATE write_memo')
   end
 
   def edit(memo_id, title, content)
-    connection.prepare('edit_memo', 'UPDATE MEMOS SET(TITLE, CONTENT) = ($1, $2) WHERE ID = $3;')
+    connection.prepare('edit_memo', 'UPDATE memos SET(title, content) = ($1, $2) WHERE id = $3;')
     connection.exec_prepared('edit_memo', [title, content, memo_id])
-    connection.exec('DEALLOCATE EDIT_MEMO')
+    connection.exec('DEALLOCATE edit_memo')
   end
 
   def delete(memo_id)
-    connection.prepare('delete_memo', 'DELETE FROM MEMOS WHERE ID = $1;')
+    connection.prepare('delete_memo', 'DELETE FROM memos WHERE id = $1;')
     connection.exec_prepared('delete_memo', [memo_id])
-    connection.exec('DEALLOCATE DELETE_MEMO')
+    connection.exec('DEALLOCATE delete_memo')
   end
 end
